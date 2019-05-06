93ecbfc028912d4a65f0475d80cb65ea4347ed22
backmeupplz <backmeupplz@gmail.com>
2019-05-06 07:40:12 -0700

    limit leaderboard to 10

--- src/helpers/leaderboard.ts
+++ src/helpers/leaderboard.ts
@@ -9,6 +9,7 @@ async function updateLeaderboard() {
     leaderboard = (await UserModel.find().limit(10))
       .map(u => u.strippedAndFilled())
       .sort((a, b) => (a._doc.overallBalance > b._doc.overallBalance ? -1 : 1))
+      .slice(0, 10)
   } catch (err) {
     report(err)
   }
