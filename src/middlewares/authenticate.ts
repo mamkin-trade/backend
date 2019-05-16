68de7df28124f15aca5c42a9750977372b728c29
backmeupplz <backmeupplz@gmail.com>
2019-05-15 20:22:57 -0700

    fix auth

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -9,14 +9,13 @@ export async function authenticate(ctx: Context, next: Function) {
   try {
     const token = ctx.headers.token
     const payload = (await verify(token)) as any
-    console.log(payload)
     let user: InstanceType<User>
     if (payload.email) {
       user = await UserModel.findOne({ email: payload.email })
     } else if (payload.facebookId) {
-      user = await UserModel.findOne({ facebookId: payload.facebookId })
+      user = await UserModel.findOne({ facebookId: `${payload.facebookId}` })
     } else if (payload.telegramId) {
-      user = await UserModel.findOne({ telegramId: payload.telegramId })
+      user = await UserModel.findOne({ telegramId: `${payload.telegramId}` })
     }
     if (!user) {
       return ctx.throw(403, errors.noUser)
