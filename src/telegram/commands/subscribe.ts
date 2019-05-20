5ecffc03b2eb46a7744fbf2ade9eb9874f72b95b
backmeupplz <backmeupplz@gmail.com>
2019-05-20 12:29:29 -0700

    fix subscribe

--- src/telegram/commands/subscribe.ts
+++ src/telegram/commands/subscribe.ts
@@ -4,7 +4,7 @@ import { attachUser } from '../helpers/checkId'
 import { subscribe } from '../../helpers/subscribe'
 
 export function setupSubscribe(bot: Telegraf<ContextMessageUpdate>) {
-  bot.start(attachUser('subscribe'), async ctx => {
+  bot.command('subscribe', attachUser('subscribe'), async ctx => {
     await subscribe(ctx.chat.id, ctx.dbuser)
     await ctx.reply(ctx.i18n.t('done'), {
       reply_to_message_id: (ctx.message || ctx.channelPost).message_id,
