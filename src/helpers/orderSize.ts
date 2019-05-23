cc4e764246c7b2347bc30e8ef74864b38f2ff978
backmeupplz <backmeupplz@gmail.com>
2019-05-22 22:17:06 -0700

    add sizes for stocks

--- src/helpers/orderSize.ts
+++ src/helpers/orderSize.ts
@@ -1,12 +1,16 @@
 // Dependencies
 import { tickers } from './bitfinex'
 import { Big } from 'big.js'
+import { isCrypto } from './isCrypto'
 
 export function minimumOrderSize(currency: string) {
   for (const ticker of Object.values(tickers)) {
     if (ticker.pair.toUpperCase() === currency.toUpperCase()) {
       return new Big(ticker.minimumOrderSize)
     }
+  }
+  if (!isCrypto(currency)) {
+    return 1
   }
   throw new Error()
 }
@@ -16,6 +20,9 @@ export function maximumOrderSize(currency: string) {
     if (ticker.pair.toUpperCase() === currency.toUpperCase()) {
       return new Big(ticker.maximumOrderSize)
     }
+  }
+  if (!isCrypto(currency)) {
+    return 1000000000
   }
   throw new Error()
 }
