2a6d067369695f3f7da3f6f84644f898efbc792c
backmeupplz <backmeupplz@gmail.com>
2019-05-31 07:58:33 -0700

    add restrictions for stocks

--- src/helpers/checkNasdaqTime.ts
+++ src/helpers/checkNasdaqTime.ts
@@ -1,11 +1,22 @@
 export function checkNasdaqTime() {
-  return true
+  const nyTime = getNewYorkTime()
+  const weekDay = nyTime.getDay()
+  if (weekDay === 0 || weekDay === 6) {
+    return false
+  }
+  const hour = nyTime.getHours()
+  if (hour >= 10 && hour < 16) {
+    return true
+  }
+  const minute = nyTime.getMinutes()
+  if (hour === 9 && minute >= 30) {
+    return true
+  }
+  return false
 }
 
-function calcTime() {
+function getNewYorkTime() {
   const utcDate = new Date()
   const utcMs = utcDate.getTimezoneOffset() * 60000 + utcDate.getTime()
   return new Date(3600000 * -4 + utcMs)
 }
-
-console.log(calcTime())
