6e9481b87cd4adae2dc9992f45bb7e38322bc361
backmeupplz <backmeupplz@gmail.com>
2019-05-31 07:52:10 -0700

    change to ny time

--- src/helpers/checkNasdaqTime.ts
+++ src/helpers/checkNasdaqTime.ts
@@ -5,7 +5,7 @@ export function checkNasdaqTime() {
 function calcTime() {
   const utcDate = new Date()
   const utcMs = utcDate.getTimezoneOffset() * 60000 + utcDate.getTime()
-  return new Date(3600000 * -5 + utcMs)
+  return new Date(3600000 * -4 + utcMs)
 }
 
 console.log(calcTime())
